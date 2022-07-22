13dbaef283ddbdcdc9f19a2cec2b833e98514e08
Aqil <testuser-aqil@protonmail.com>
2022-07-22 16:35:12 +0400

    fix: timezone issue

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+from datetime import datetime
 import logging
-import logging.handlers
+import time
+from pytz import timezone, utc
 
 import requests
 
+def customTime(*args):
+        utc_dt = utc.localize(datetime.utcnow())
+        my_tz = timezone("Asia/Baku")
+        converted = utc_dt.astimezone(my_tz)
+        return converted.timetuple()
+logging.Formatter.converter = customTime
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "data.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8"
-)
+logger_file_handler = logging.FileHandler("data.log", encoding="utf8")
 formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 logger_file_handler.setFormatter(formatter)
 logger.addHandler(logger_file_handler)
