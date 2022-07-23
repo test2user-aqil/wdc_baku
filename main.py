853c7689921b903b1c3aa8928bde7a53d82feae4
Aqil <testuser-aqil@protonmail.com>
2022-07-23 18:39:28 +0400

    add formatter

--- main.py
+++ main.py
@@ -8,16 +8,21 @@ import json
 
 tz_Baku = timezone("Asia/Baku")
 
+
 def customTime(*args):
-        utc_dt = utc.localize(datetime.utcnow())
-        converted = utc_dt.astimezone(tz_Baku)
-        return converted.timetuple()
+    utc_dt = utc.localize(datetime.utcnow())
+    converted = utc_dt.astimezone(tz_Baku)
+    return converted.timetuple()
+
+
 logging.Formatter.converter = customTime
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger_file_handler = logging.FileHandler("data/data.log", encoding="utf8")
-formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", datefmt='%d-%m-%Y %H:%M:%S')
+formatter = logging.Formatter(
+    "%(asctime)s - %(levelname)s - %(message)s", datefmt="%d-%m-%Y %H:%M:%S"
+)
 logger_file_handler.setFormatter(formatter)
 logger.addHandler(logger_file_handler)
 
@@ -31,16 +36,28 @@ if __name__ == "__main__":
         description = data["weather"]["description"]
         logger.info(f"Weather in Baku: {temperature}°C - {description}")
 
-        with open('data/data.csv',  mode='a') as csv_file:
-            fieldnames = ['date_time', 'temperature', 'description']
+        with open("data/data.csv", mode="a") as csv_file:
+            fieldnames = ["date_time", "temperature", "description"]
             writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
-            writer.writerow({'date_time': datetime.now(tz_Baku).strftime("%d-%m-%Y %H:%M:%S"), 'temperature': temperature, 'description': description})
-        
-        with open('data/data.json', 'r') as f:
+            writer.writerow(
+                {
+                    "date_time": datetime.now(tz_Baku).strftime("%d-%m-%Y %H:%M:%S"),
+                    "temperature": temperature,
+                    "description": description,
+                }
+            )
+
+        with open("data/data.json", "r") as f:
             json_data = json.load(f)
 
-        json_data.append({'date_time': datetime.now(tz_Baku).strftime("%d-%m-%Y %H:%M:%S"), 'temperature': temperature, 'description': description})
-        
-        with open('data/data.json', 'w') as f:
-            json.dump(json_data, f, indent=4, separators=(',',': '))
+        json_data.append(
+            {
+                "date_time": datetime.now(tz_Baku).strftime("%d-%m-%Y %H:%M:%S"),
+                "temperature": temperature,
+                "description": description,
+            }
+        )
+
+        with open("data/data.json", "w") as f:
+            json.dump(json_data, f, indent=4, separators=(",", ": "))
