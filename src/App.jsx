c040710fe7f2a974ecc925ef6457df0004acd9d7
Aqil <testuser-aqil@protonmail.com>
2022-08-06 12:30:23 +0400

    homepage in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./index.css";
 
 function App() {
-    return <div className="text-3xl font-bold underline">test</div>;
+    return (
+        <div className="bg-gray-900 min-h-screen text-blue-200 justify-center items-start flex">
+            <div className="mt-10 font-mono text-3xl">
+                Weather Data Collector <b>Baku</b>
+            </div>
+        </div>
+    );
 }
 
 export default App;
