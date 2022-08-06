6ee69547e1941a50ade59def264ca41153d1ff11
Aqil <testuser-aqil@protonmail.com>
2022-08-06 11:10:10 +0400

    format files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/weatherdatacollector_baku/',
-  plugins: [react()]
-})
+    base: "/weatherdatacollector_baku/",
+    plugins: [react()],
+});
