3ad2d46ef73c0697dcc3da5ec243865e124a62fa
Aqil <testuser-aqil@protonmail.com>
2022-08-07 15:22:27 +0400

    .jsx -> .js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import WeatherChart from "./components/WeatherChart";
 function App() {
     return (
         <div className="text-fg max-w-5xl mx-auto justify-start items-center flex flex-col gap-10 py-16">
-            <div className="font-mono text-5xl">
+            <div className="font-mono lg:text-5xl md:text-4xl text-3xl transition-all duration-300">
                 Weather Data Collector <span className="underline">Baku</span>
             </div>
-            {/* <WeatherChart /> */}
         </div>
     );
 }
---
+++ src/components/WeatherChart.js
@@ -0,0 +1,26 @@
+import Chart from "chart.js/auto";
+import { Line } from "react-chartjs-2";
+
+function WeatherChart() {
+    return (
+        <> ) </>
+        // <Line
+        //     width={1200}
+        //     height={600}
+        //     data={{
+        //         labels: data.map((day) => day.date_time),
+        //         datasets: [
+        //             {
+        //                 label: "temperature",
+        //                 data: data.map((day) => day.temperature),
+        //                 borderWidth: 1,
+        //                 fill: true,
+        //                 tension: 0.5,
+        //             },
+        //         ],
+        //     }}
+        // />
+    );
+}
+
+export default WeatherChart;
