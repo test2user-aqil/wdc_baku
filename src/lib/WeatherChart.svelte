80a1afa97849ee010e1d70a16dc7923375473c54
Aqil <testuser-aqil@protonmail.com>
2022-08-08 19:15:56 +0400

    fix: use import instead of fetch

--- src/lib/WeatherChart.svelte
+++ src/lib/WeatherChart.svelte
@@ -1,6 +1,7 @@
 <script>
     import Chart from "chart.js";
     import { onMount } from "svelte";
+    import data from "../../public/data/data.json";
 
     export let day, month, year;
 
@@ -9,22 +10,23 @@
     let ctx;
     let chartCanvas;
 
-    onMount(async (promise) => {
-        const response = await fetch("/data/data.json");
-        const result = await response.json();
-        await result.map((i) => {
+    const get = async () => {
+        data.map((i) => {
             if (
                 parseInt(i.date_time.slice(6, 10)) === year &&
                 parseInt(i.date_time.slice(3, 5)) === month &&
                 parseInt(i.date_time.slice(0, 2)) === day
             ) {
-                chartValues.push(parseInt(i.temperature));
+                chartValues.push(i.temperature);
                 chartLabels.push(i.date_time.slice(10, 16));
             }
         });
+    };
 
+    onMount(async () => {
+        await get();
         ctx = await chartCanvas.getContext("2d");
-        var chart = new Chart(ctx, {
+        var chart = await new Chart(ctx, {
             type: "line",
             data: {
                 labels: chartLabels,
