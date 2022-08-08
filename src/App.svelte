82568332ad731088862f34c80bc96725bdb9108d
Aqil <testuser-aqil@protonmail.com>
2022-08-08 10:51:57 +0400

    Add chart

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
-    import Counter from "./lib/Counter.svelte";
     import Hero from "./lib/Hero.svelte";
+    import WeatherChart from "./lib/WeatherChart.svelte";
 </script>
 
 <main
     class="text-fg max-w-5xl mx-auto justify-start items-center flex flex-col gap-10 py-16"
 >
     <Hero />
+    <WeatherChart />
 </main>
---
+++ src/lib/WeatherChart.svelte
@@ -0,0 +1,44 @@
+<script>
+    import { Line } from "svelte-chartjs";
+
+    let count = 0;
+</script>
+
+<input type="number" name="" id="" bind:value={count} />
+
+<Line
+    data={{
+        labels: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+        ],
+        datasets: [
+            {
+                label: "My First dataset",
+                fill: true,
+                tension: 0.3,
+                backgroundColor: "rgba(225, 204,230, .3)",
+                borderColor: "rgb(205, 130, 158)",
+                borderCapStyle: "butt",
+                borderDash: [],
+                borderDashOffset: 0.0,
+                borderJoinStyle: "miter",
+                pointBorderColor: "rgb(205, 130,1 58)",
+                pointBackgroundColor: "rgb(255, 255, 255)",
+                pointBorderWidth: 10,
+                pointHoverRadius: 5,
+                pointHoverBackgroundColor: "rgb(0, 0, 0)",
+                pointHoverBorderColor: "rgba(220, 220, 220,1)",
+                pointHoverBorderWidth: 2,
+                pointRadius: 1,
+                pointHitRadius: 10,
+                data: [count, 59, 80, 81, 56, 55, 40],
+            },
+        ],
+    }}
+/>
