e843b2886844161dd3f4877673c3452682fd0a95
Aqil <testuser-aqil@protonmail.com>
2022-08-08 09:51:21 +0400

    from react to svelte

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: "/weatherdatacollector_baku/",
-    plugins: [react()],
+    plugins: [svelte()],
 });
