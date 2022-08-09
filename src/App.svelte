cf744ed928b50986a4eedd9505653607e7a36ca9
Aqil <testuser-aqil@protonmail.com>
2022-08-09 11:00:37 +0400

    Create DateSwitcher

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script>
     import Hero from "./lib/Hero.svelte";
     import WeatherChart from "./lib/WeatherChart.svelte";
+    import DateSwitcher from "./lib/DateSwitcher.svelte";
 </script>
 
 <main
     class="text-fg max-w-5xl mx-auto justify-start items-center flex flex-col gap-10 py-16"
 >
     <Hero />
-    <WeatherChart day={1} month={8} year={2022}/>
+    <DateSwitcher />
+    <WeatherChart day={1} month={8} year={2022} />
 </main>
---
+++ src/lib/DateSwitcher.svelte
@@ -0,0 +1,13 @@
+<script>
+    let date;
+
+    $: console.log(date);
+</script>
+
+<input
+    type="date"
+    name=""
+    id=""
+    class="bg-dark0 border-4 border-fg text-fg h-16 py-5 px-14 rounded-full font-mono flex items-center justify-center"
+    bind:value={date}
+/>
