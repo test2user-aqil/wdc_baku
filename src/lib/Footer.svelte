5e3f912df2841cf2f0a99ca050b50587b93a709f
Aqil <testuser-aqil@protonmail.com>
2022-08-11 10:02:40 +0400

    Add a little bit style

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -4,7 +4,9 @@
 <footer
     class="font-display bg-dark0 shadow-xl shadow-accent1/5 w-full h-10 lg:rounded-2xl box-shadow flex justify-center items-center font-medium mt-auto lg:my-4 rounded-none transition-all duration-300"
 >
-    <div class="text-fg">
+    <div
+        class="text-fg relative after:absolute after:inset-0 after:-z-10 after:blur-[1000px] after:bg-accent1 after:-translate-y-10 after:rounded-full after:h-[80px]"
+    >
         Copyright &copy; <span id="copyrightYear"
             >{new Date().getFullYear()}</span
         >
--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -9,7 +9,7 @@
     class="font-mono tracking-tight text-center p-4 lg:text-5xl md:text-4xl text-3xl transition-all duration-300"
 >
     Weather Data Collector <span
-        class="relative underline after:absolute after:inset-0 after:-z-10 after:blur-[120px] after:bg-accent1 after:rounded-full after:w-[200px] after:h-[80px]"
+        class="relative underline after:absolute after:inset-0 after:-z-10 after:blur-[120px] after:bg-accent1/90 after:-translate-x-1/4 after:rounded-full after:w-[300px] after:h-[100px]"
         >Baku</span
     >
 </h1>
