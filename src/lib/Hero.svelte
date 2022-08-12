384ed51d52f7fdae0ac695aaa599e728ace32bf2
Aqil <testuser-aqil@protonmail.com>
2022-08-12 15:12:47 +0400

    a little bit style

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -6,7 +6,7 @@
 />
 
 <h1
-    class="font-mono tracking-tight text-center p-4 lg:text-5xl md:text-4xl text-3xl transition-all duration-300"
+    class="font-mono tracking-tight text-center p-4 lg:text-5xl md:text-4xl text-3xl transition-all duration-300 md:after:absolute after:inset-0 after:-z-10 after:blur-[120px] after:bg-accent1/90 after:rounded-full after:w-[30px] after:h-96 relative after:top-0 after:-left-10"
 >
     Weather Data Collector <span
         class="relative underline after:absolute after:inset-0 after:-z-10 after:blur-[120px] after:bg-accent1/90 after:-translate-x-1/4 after:rounded-full after:w-[300px] after:h-[100px]"
