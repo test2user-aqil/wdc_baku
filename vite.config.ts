5f43d687c25839b8908b209fb08f5977295fb0b2
Aqil <testuser-aqiL@protonmail.com>
2023-01-07 02:59:16 +0400

    Delete files

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    base: "/wdc_baku/",
-    plugins: [svelte()]
-});
